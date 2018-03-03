e9e02366a18ddb820c583d0c1a5e24c28f04a696
Elon <yulong5201@gmail.com>
2018-03-03 11:11:50 +0800

    Initial commit

---
+++ testPolls/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question,Choice
+
+# admin.site.register((Question,Choice))
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date','question_text']
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date information',{'fields':['pub_date']}),
+    ]
+
+admin.site.register(Question,QuestionAdmin)
---
+++ testPolls/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TestpollsConfig(AppConfig):
+    name = 'testPolls'
